c744e02970e8e781b5654f11ede12d3523ffff06
AndrewKizito <AndrewKizito54@gmail.com>
2023-04-24 11:33:59 +0300

    Screens - Home Added

---
+++ screens/Home/Home.tsx
@@ -0,0 +1,15 @@
+// Importing core components
+import { StyleSheet, Text } from "react-native"
+import Layout from "../../components/core/Layout"
+
+const Home = () => {
+  return (
+    <Layout>
+      <Text>Home</Text>
+    </Layout>
+  )
+}
+
+export default Home
+
+const styles = StyleSheet.create({})
