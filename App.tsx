a595b6775b44a289c1c3d5ae0887d39d0dde0a2e
AndrewKizito <AndrewKizito54@gmail.com>
2023-04-24 11:33:04 +0300

    App Setup

--- App.tsx
+++ App.tsx
@@ -1,20 +1,41 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// Importing helper modules
+import { useCallback } from "react"
+import { useFonts } from "expo-font"
+import * as SplashScreen from "expo-splash-screen"
+
+// Importing core components
+import { View } from "react-native"
+
+// Importing root navigator
+import Main from "./routers/Main"
+
+// SplashScreen.preventAutoHideAsync()
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+	// Loading fonts
+	const [fontsLoaded] = useFonts({
+		"nunito-200": require("./assets/fonts/Nunito-200.ttf"),
+		"nunito-300": require("./assets/fonts/Nunito-300.ttf"),
+		"nunito-400": require("./assets/fonts/Nunito-400.ttf"),
+		"nunito-500": require("./assets/fonts/Nunito-500.ttf"),
+		"nunito-600": require("./assets/fonts/Nunito-600.ttf"),
+		"nunito-700": require("./assets/fonts/Nunito-700.ttf"),
+	})
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+	// Hide splash screen on successful font-load
+	const onLayoutRootView = useCallback(async () => {
+		if (fontsLoaded) {
+			await SplashScreen.hideAsync()
+		}
+	}, [fontsLoaded])
+
+	if (!fontsLoaded) {
+		return null
+	}
+
+	return (
+		<View style={{ flex: 1 }} onLayout={onLayoutRootView}>
+			<Main />
+		</View>
+	)
+}
